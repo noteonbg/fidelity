157fb2049644871e7e73bc4619e74cbe3a79e719
vasu <vasubg@outlook.com>
2024-07-29 08:47:29 +0530

    updated

---
+++ code/taskscenario/TestTask.java
@@ -0,0 +1,16 @@
+package taskscenario;
+
+import taskscenario.controller.TaskPresentation;
+
+public class TestTask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1);
+		TaskPresentation taskPresentation =new TaskPresentation();
+		taskPresentation.start();
+		
+
+	}
+
+}
---
+++ readme.txt
@@ -0,0 +1,8 @@
+github link
+
+https://github.com/noteonbg/fidelity.git
+
+
+doubts link.
+https://forms.gle/bpvf2rWnEonTVsqG9
+
